f358d366b3bab8441f94ae4e947bc17ce48b017a
Arjun Rajan <arjunrajan991@gmail.com>
2025-02-05 22:46:24 +0530

    handling if any token in request line is NULL, added error response code html files and fucntions

--- response.c
+++ response.c
@@ -4,6 +4,8 @@
 #include "utilities.h"
 
 int send_apt_response(char *path);
+void send_505_response();
+void send_400_response();
 
 int process_response(int cfd, char *request_line)
 {
@@ -14,6 +16,14 @@ int process_response(int cfd, char *request_line)
     char *uri = strtok_r(NULL, " ", &token_ptr);
     char *http_ver = strtok_r(NULL, " ", &token_ptr);
 
+    if (method == NULL || uri == NULL || http_ver == NULL)
+    {
+        int code = 400;
+        send_400_response();
+        printf("%d Bad Request.\n", code);
+        return code;
+    }
+
     printf("Method: %s, URI: %s, HTTP Version: %s.\n", method, uri, http_ver);
 
     if (check_method(method) == 0)
@@ -21,7 +31,7 @@ int process_response(int cfd, char *request_line)
         if (strcmp(method, "GET") != 0)
         {
             int code = 501;
-            //send_501_response(char *uri);
+            //send_501_response();
             printf("%d Not Implemented.\n", code);
             return code;
         }
@@ -29,18 +39,22 @@ int process_response(int cfd, char *request_line)
     else 
     {
         int code = 405;
+        //send_405_response();
         printf("%d Method Not Allowed.\n", code);
         return code;
     }
-    if (uri[0] != '/')
-    {
-        int code = 400;
-        printf("%d Bad Request.\n", code);
-        return code;
-    }
+    // No need to check uri[0] is backslash, instead check for invalid char.
+    //if (uri[0] != '/')
+    //{
+    //    int code = 400;
+    //    send_400_response();
+    //    printf("%d Bad Request.\n", code);
+    //    return code;
+    //}
     if (strcmp(http_ver, "HTTP/1.1") != 0 && strcmp(http_ver, "HTTP/1.0") != 0)
     {
         int code = 505;
+        send_505_response();
         printf("%d HTTP Version Not Supported.\n", code);
         return code;
     }
@@ -74,34 +88,70 @@ int send_apt_response(char *path)
     }
 }
 
-char *index_html_response() {
 
-    const char *http_header = "HTTP/1.0 200 OK\r\n"
+//void send_501_response();
+//void send_405_response();
+
+
+void send_400_response()
+{
+
+    const char *http_header = "HTTP/1.0 400 Bad Request.\r\n"
                               "Content-Type: text/html\r\n"
-                              "Content-Length: %lu\r\n"
                               "Connection: close\r\n\r\n";
 
-    FILE *index_html = fopen("server_root/index.html", "r");
-    if (index_html == NULL) {
+    FILE *html_file = fopen("server_root/error/400.html", "r");
+    if (html_file == NULL) {
         perror("Error opening file");
-        return NULL;
     }
 
-    fseek(index_html, 0L, SEEK_END);
-    unsigned long file_size = ftell(index_html);
-    fseek(index_html, 0L, SEEK_SET);
+    fseek(html_file, 0L, SEEK_END);
+    unsigned long file_size = ftell(html_file);
+    fseek(html_file, 0L, SEEK_SET);
 
     size_t total_response_size = file_size + strlen(http_header) + 1;
-    char *http_response = (char *)malloc(total_response_size);
+    char *http_response = malloc(total_response_size);
 
-    sprintf(http_response, http_header, file_size);
+    strcpy(http_response, http_header);
 
-    size_t bytes_read = fread(http_response + strlen(http_header), 1,file_size, index_html);
-    fclose(index_html);
-    
-    return http_response;
+    size_t bytes_read = fread(http_response, 1, file_size, html_file);
+    printf("%s\n.",http_response);
+
+    fclose(html_file);
+    free(http_response);
 
 }
+
+void send_505_response()
+{
+
+    const char *http_header = "HTTP/1.0 505 HTTP Version Not Supported.\r\n"
+                              "Content-Type: text/html\r\n"
+                              "Connection: close\r\n\r\n";
+
+    FILE *html_file = fopen("server_root/error/505.html", "r");
+    if (html_file == NULL) {
+        perror("Error opening file");
+    }
+
+    fseek(html_file, 0L, SEEK_END);
+    unsigned long file_size = ftell(html_file);
+    fseek(html_file, 0L, SEEK_SET);
+
+    size_t total_response_size = file_size + strlen(http_header) + 1;
+    char *http_response = malloc(total_response_size);
+
+    strcpy(http_response, http_header);
+
+    size_t bytes_read = fread(http_response, 1, file_size, html_file);
+    printf("%s\n.",http_response);
+
+    fclose(html_file);
+    free(http_response);
+
+}
+
+
 
 char* generate_image_header() {
     char *http_header = "HTTP/1.0 200 OK\r\n"
