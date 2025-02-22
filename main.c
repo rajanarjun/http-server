3c197fedda74b88d94d012847a74a55e41335949
Arjun Rajan <arjunrajan991@gmail.com>
2025-02-23 01:47:38 +0530

    got error pages to serve, minor syntax change in main, moved all images to images/

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        perror("[Error] Please port for the server");
+        perror("[Error] Please enter port for the server");
         return 1;
     }
    
--- response.c
+++ response.c
@@ -9,7 +9,7 @@
 #define HEADER_MAX_SIZE 8192
 
 void send_ok_response(int cfd, char *path);
-void send_error_response(int error_code);
+void send_error_response(int cfd, int error_code);
 
 char *METHOD_TOKENS[TOKENS_LENGTH] = {"OPTIONS", 
                                       "GET",
@@ -47,7 +47,7 @@ void process_response(int cfd, char *request_line)
     if (method == NULL || uri == NULL || http_ver == NULL)
     {
         code = 400;
-        send_error_response(code);
+        send_error_response(cfd, code);
         printf("%d Bad Request.\n", code);
     }
 
@@ -59,14 +59,14 @@ void process_response(int cfd, char *request_line)
         if (strcmp(method, "GET") != 0)
         {
             code = 501;
-            send_error_response(code);
+            send_error_response(cfd, code);
             printf("%d Not Implemented.\n", code);
         }
     }
     else 
     {
         code = 405;
-        send_error_response(code);
+        send_error_response(cfd, code);
         printf("%d Method Not Allowed.\n", code);
     }
     
@@ -74,7 +74,7 @@ void process_response(int cfd, char *request_line)
     if (strcmp(http_ver, "HTTP/1.0") != 0 && strcmp(http_ver, "HTTP/1.1") != 0)
     {
         code = 505;
-        send_error_response(code);
+        send_error_response(cfd, code);
         printf("%d HTTP Version Not Supported.\n", code);
     }
     
@@ -83,7 +83,7 @@ void process_response(int cfd, char *request_line)
     if (uri[0] != '/')
     {
         code = 400;
-        send_error_response(code);
+        send_error_response(cfd, code);
         printf("%d Bad Request: invalid start to path.\n", code);
     }
 
@@ -91,7 +91,7 @@ void process_response(int cfd, char *request_line)
     if (strstr(uri, "../") != NULL)
     {
         code = 400;
-        send_error_response(code);
+        send_error_response(cfd, code);
         printf("%d Bad Request: dangerous path traversal.\n", code);
     }
 
@@ -102,7 +102,7 @@ void process_response(int cfd, char *request_line)
         if ( uri[i] == '\\' || ( (uri[i] == '%') && (!isxdigit(uri[i+1]) || !isxdigit(uri[i+2])) ) )
         {
             code = 400;
-            send_error_response(code);
+            send_error_response(cfd, code);
             printf("%d Bad Request: invalid character or percend-encoding in URI.\n", code);
         }
     }
@@ -110,45 +110,46 @@ void process_response(int cfd, char *request_line)
     send_ok_response(cfd, uri);
 }
 
-void send_error_response(int error_code)
+void send_error_response(int cfd, int error_code)
 {
-    const char *error_directory = "server_root/error/";
-    const char *requested_file;
-    const char *http_header;
+    const char *error_directory = "server_root/error_pages/";
+    char *requested_file;
+    char http_header[HEADER_MAX_SIZE];
+    char *temp_header;
 
     switch (error_code)
     {
         case 400:
             requested_file = "400.html";
-            http_header = "HTTP/1.0 400 Bad Request.\r\n"
+            temp_header = "HTTP/1.0 400 Bad Request.\r\n"
                               "Content-Type: %s\r\n"
                               "Content-Length: %d\r\n"
                               "Connection: close\r\n\r\n";
             break;
         case 404:
             requested_file = "404.html"; 
-            http_header = "HTTP/1.0 404 Not Found.\r\n"
+            temp_header = "HTTP/1.0 404 Not Found.\r\n"
                               "Content-Type: %s\r\n"
                               "Content-Length: %d\r\n"
                               "Connection: close\r\n\r\n";
             break;
         case 405:
             requested_file = "405.html"; 
-            http_header = "HTTP/1.0 405 Method Not Allowed.\r\n"
+            temp_header = "HTTP/1.0 405 Method Not Allowed.\r\n"
                               "Content-Type: %s\r\n"
                               "Content-Length: %d\r\n"
                               "Connection: close\r\n\r\n";
             break;
         case 501:
             requested_file = "501.html"; 
-            http_header = "HTTP/1.0 501 Not Implemened.\r\n"
+            temp_header = "HTTP/1.0 501 Not Implemened.\r\n"
                               "Content-Type: %s\r\n"
                               "Content-Length: %d\r\n"
                               "Connection: close\r\n\r\n";
             break;
         case 505:
             requested_file = "505.html"; 
-            http_header = "HTTP/1.0 505 HTTP Version Not Supported.\r\n"
+            temp_header = "HTTP/1.0 505 HTTP Version Not Supported.\r\n"
                               "Content-Type: %s\r\n"
                               "Content-Length: %d\r\n"
                               "Connection: close\r\n\r\n";
@@ -158,8 +159,36 @@ void send_error_response(int error_code)
     char *full_path = malloc(strlen(error_directory) + strlen(requested_file) + 1);
     strcpy(full_path, error_directory);
     strcat(full_path, requested_file);
-    printf("Client requested: %s.\n", full_path);
-    free(full_path);
+
+    FILE *file_to_send = open_file(full_path);
+    if (file_to_send == NULL) 
+    {
+        free(full_path);
+        perror("[Error] Custom error page not found\n");
+    }
+    else
+    {
+        printf("Client requested: %s\n", full_path);
+        char *file_mime_type = get_mime_type(requested_file);
+        unsigned long file_size = get_file_size(file_to_send);
+
+        sprintf(http_header, temp_header,
+                  file_mime_type,
+                  file_size);
+
+        /* printf("%s\n", http_header); */
+
+        // send the header with content type and length
+        send(cfd, http_header, strlen(http_header), 0);
+        
+        // send the file
+        char *f_data = get_file_data(file_to_send, file_size);
+        send(cfd, f_data, file_size, 0);
+
+        free(full_path);
+        free(f_data);
+        close_file(file_to_send);
+    }
 }
 
 void send_ok_response(int cfd, char *path)
@@ -171,13 +200,14 @@ void send_ok_response(int cfd, char *path)
     char *full_path = malloc(strlen(root_directory) + strlen(requested_file) + 1);
     strcpy(full_path, root_directory);
     strcat(full_path, requested_file);
-    printf("Client requested: %s.\n", full_path);
+
+    printf("Client requested: %s\n", full_path);
 
     FILE *file_to_send = open_file(full_path);
     if (file_to_send == NULL) 
     {
-        /* send_error_response(404); */
-        printf("404 not found\n");
+        free(full_path);
+        send_error_response(cfd, 404);
     }
     else
     {
@@ -192,21 +222,17 @@ void send_ok_response(int cfd, char *path)
                   file_mime_type,
                   file_size);
     
-        /* printf("%s\n", http_header); */
-        /* printf("File mime type: %s\n", file_mime_type); */
-        /* printf("%ld\n", strlen(http_header)); */
-
         // send the header with content type and length
         send(cfd, http_header, strlen(http_header), 0);
         
         // send the file
         char *f_data = get_file_data(file_to_send, file_size);
         send(cfd, f_data, file_size, 0);
-        free(f_data);
-    }
 
-    free(full_path);
-    close_file(file_to_send);
+        free(full_path);
+        free(f_data);
+        close_file(file_to_send);
+    }
 }
 
 
