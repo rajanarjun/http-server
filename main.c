d693434ced2082d3f69428d339417dc30a12012d
Arjun Rajan <arjunrajan991@gmail.com>
2025-08-13 08:43:15 +0530

    utilities header removed

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include "utilities.h"
+#include "response.h"
 
 #define REQUEST_MAX_BYTES 1024
 
--- utilities.h
+++ utilities.h
@@ -4,11 +4,6 @@
 #include <stdio.h>
 
 void process_response(int cfd, char *request_line);
-char *get_mime_type(char *filename);
 
-FILE *open_file(char *file_path);
-unsigned long get_file_size(FILE *opened_file);
-char *get_file_data(FILE *opened_file, unsigned long file_size);
-void close_file(FILE *opened_file);
 
 #endif 
