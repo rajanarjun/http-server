4bc5628db5abc3ebcdbc7a77b2863f36a039c73b
Arjun Rajan <arjunrajan991@gmail.com>
2025-08-13 08:27:13 +0530

    renamed get_file to file_info

---
+++ file_info.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "file_info.h"
+
+FILE *open_file(char *file_path)
+{
+    FILE *file = fopen(file_path, "rb"); 
+    if (file == NULL) { 
+        perror("Send 404. File not found."); 
+        return NULL; 
+    } 
+
+    return file;
+}
+
+unsigned long get_file_size(FILE *opened_file)
+{
+    fseek(opened_file, 0L, SEEK_END); 
+    unsigned long file_size = ftell(opened_file); 
+    fseek(opened_file, 0L, SEEK_SET); 
+
+    return file_size;
+}
+
+char *get_file_data(FILE *opened_file, unsigned long file_size)
+{
+    char *file_data = malloc(file_size);
+    if (file_data == NULL) {
+        printf("[Error] Could not allocate memory for file data.\n");
+        return NULL;
+    }
+    size_t bytes_read = fread(file_data, 1, file_size, opened_file);
+    if (bytes_read == 0) {
+        perror("[Error] Unable to open file.\n");
+        free(file_data);
+        return NULL;
+    }
+    return file_data;
+}
+
+void close_file(FILE *opened_file)
+{
+    fclose(opened_file);
+}
+
